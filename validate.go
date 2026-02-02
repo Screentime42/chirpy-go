3ebb5d8417e8078636ad6180c54eafbe184f81a0
Screentime42 <danohare99@googlemail.com>
2026-02-02 12:36:01 +0000

    Added censored words validation

--- validate.go
+++ validate.go
@@ -3,7 +3,34 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
+
+// List to hold banned words
+var bannedWords = map[string]struct{}{
+	"kerfuffle": {},
+	"sharbert": {},
+	"fornax": {},
+}
+
+
+// function to replace banned words with specified replacement
+func censorBannedWords(body string, banned map[string]struct{}, replacement string) string {
+	words := strings.Fields(body)
+	out := make([]string, len(words))
+
+	for i, w := range words {
+		lower := strings.ToLower(w)
+
+		if _, banned := banned[lower]; banned {
+			out[i] = replacement
+		} else {
+			out[i] = w
+		}
+	}
+	return strings.Join(out, " ")
+}
+
 
 // handlerValidate validates a chirp sent in the request body
 // It ensures the JSON is valid and the chirp meets length requirements
@@ -30,11 +57,15 @@ func handlerValidate (w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Apply censor
+	cleaned := censorBannedWords(params.Body, bannedWords, "****")
+
 	// Respond with a simple JSON payload indicating success
-	respondWithJSON(w, http.StatusOK, map[string]bool{
-		"valid": true,
+	respondWithJSON(w, http.StatusOK, map[string]string{
+		"cleaned_body": cleaned,
 	})
 }
+
 
 // respondWithError sends a JSON‑formatted error message with the given status code 
 // Used for consistent error responses across the API
