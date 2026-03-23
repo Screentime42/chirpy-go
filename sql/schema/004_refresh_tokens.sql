42069187265dc8ca4d93350b8b67c8f36dc4dd03
Screentime42 <danohare99@googlemail.com>
2026-03-23 11:49:12 +0000

    added refresh tokens table to schema

---
+++ sql/schema/004_refresh_tokens.sql
@@ -0,0 +1,12 @@
+-- +goose up
+CREATE TABLE refresh_tokens (
+   token VARCHAR(255) PRIMARY KEY,
+   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+   updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+   user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+   expires_at TIMESTAMP NOT NULL,
+   revoked_at TIMESTAMP
+);
+
+-- +goose down
+DROP TABLE refresh_tokens;
